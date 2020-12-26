0ed77dedc5cc3f7131a82592f8812504e1cccda5
sechan94 <71137681+sechan94@users.noreply.github.com>
2020-12-27 00:19:53 +0900

    Add files via upload

--- GUI/StuLectureList.java
+++ GUI/StuLectureList.java
@@ -6,9 +6,6 @@ import java.awt.event.ActionListener;
 import java.util.*;
 import javax.swing.*;
 import javax.swing.table.*;
-import javax.swing.JTabbedPane;
-
-
 
 import Calcul.Calcul;
 import database.AjouinDataBase;
@@ -37,36 +34,64 @@ public class StuLectureList extends JFrame {
     
     public StuLectureList(){
     
-        EnrolmentManager em = new EnrolmentManager();
+       
         DataBase<Ajouin> DB_ajouin = AjouinDataBase.getDB();
         DataBase<Subject> DB_subject = SubjectDataBase.getDB();
         DataBase<Lecture> DB_lecture = LectureDataBase.getDB();
         
         setTitle("수강신청 목록");
-        setSize(1500, 1000);
+        setSize(1300, 1000);
         setLocationRelativeTo(null); 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      
-        JPanel jp1 = new JPanel();
-        JPanel jp2 = new JPanel();
+        JPanel panel = new JPanel();
 
-
-
-
-
-
-    
-        JPanel panel = new JPanel(); //수강신청 쪽 패널
+        
+        String col[] = { "강의명","과목코드", "학점", "여석", "강의실","강의시간" };   //필드명(열제목) 지정
+        
+        Object values[][] = { { "수학", "123", "3", "4","팔달","3:00~9:00" }, //레코드값! 7개의 레코드 생성
+                {  "수학", "123", "3", "4","팔달","3:00~9:00" },
+                {  "수학", "123", "3", "4","팔달","3:00~9:00" },
+                {  "수학", "123", "3", "4","팔달","3:00~9:00" },
+                {  "수학", "123", "3", "4","팔달","3:00~9:00" },
+                {  "수학", "123", "3", "4","팔달","3:00~9:00" },
+                {  "수학", "123", "3", "4","팔달","3:00~9:00" } };
+        
+        JLabel title = new JLabel("아주대학교 정보통신대학 수강신청 프로그램");
+        
+       
+        
+       
+        JTable table = new JTable(values, col);//수강신청 테이블
+        
+        JScrollPane pane = new JScrollPane(table); 
+        panel.add(pane); 
+        pane.setLocation(100,100);
+        pane.setSize(800,750);
+        
         JButton btn1 = new JButton("수강 등록");
         JButton btn2 = new JButton("수강 취소");
         JButton btn3 = new JButton("학점 계산기");
         JButton btn4 = new JButton("신청내역 확인");
+        panel.setLayout(null); // 버튼 고정 레이아웃 해제
+        
+      
+        btn1.setBounds(100, 900, 122, 30);
+        btn2.setBounds(300, 900, 122, 30);
+        btn3.setBounds(500, 900, 122, 30);
+        btn4.setBounds(700, 900, 122, 30); 
+        
+        
+
         panel.add(btn1);
         panel.add(btn2);
         panel.add(btn3);
         panel.add(btn4);
         this.add(panel);
-     
+        panel.add(title);
+        setTitle("아주대학교 정통대 수강신청");
+        
+        
+        
         
 
 
@@ -122,7 +147,15 @@ public class StuLectureList extends JFrame {
 
 
         setVisible(true);
+        
 
+    
     }
+    public static void main(String[] args)
+    {
+    	new StuLectureList();
+    }
+
+
 }// 메인 프레임
 
